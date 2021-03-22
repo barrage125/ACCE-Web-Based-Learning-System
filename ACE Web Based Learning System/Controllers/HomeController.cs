1201d2079d80c9d48fe4669460d8d0f438e2f594
ericlieu12 <ericlieu118@gmail.com>
2021-03-22 12:40:40 -0400

    Initial commit

---
+++ ACE Web Based Learning System/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACE_Web_Based_Learning_System.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var db = new masterEntities();
+            Console.WriteLine(db.Users);
+            Console.WriteLine(db.CourseContent);
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ACE Web Based Learning System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ACE_Web_Based_Learning_System.MvcApplication" Language="C#" %>
